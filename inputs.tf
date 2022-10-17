e1956e24e58640560a1dbda2ea78ea8098267738
ns <namgaddasatish@gmail.com>
2022-10-17 14:37:34 +0530

    inputs.tf

--- inputs.tf
+++ inputs.tf
@@ -1,8 +1,8 @@
 variable "target_region" {
     type      = string
-    default   =  "us-west-2"
+   # default   =  "us-west-2"
 }
 variable "vpc_range" {
     type      = string
-   default    = "10.10.0.0/16"
+   #default    = "10.10.0.0/16"
 }
