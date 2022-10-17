64dee29b9b8e23b0cfcef92cab00bf287409c1bf
ns <namgaddasatish@gmail.com>
2022-10-17 23:53:37 +0530

    changes in .tf

--- inputs.tf
+++ inputs.tf
@@ -1,8 +1,32 @@
 variable "target_region" {
-    type      = string
-   # default   =  "us-west-2"
+    type    = string
+    #default = "us-west-2"
 }
 variable "vpc_range" {
+    type     = string
+    #default  = "10.100.0.0/16"  
+}
+variable "subnet1_range" {
     type      = string
-   #default    = "10.10.0.0/16"
+    #defaulit = "10.10.0.0/24"
+}
+variable "subnet2_range" {
+    type      = string
+    #defaulit = "10.10.0.0/24"
+}
+variable "subnet3_range" {
+    type      = string
+    #defaulit = "10.10.0.0/24"
+}
+variable "subnet4_range" {
+    type      = string
+    #defaulit = "10.10.0.0/24"
+}
+variable "subnet5_range" {
+    type      = string
+    #defaulit = "10.10.0.0/24"
+}
+variable "subnet6_range" {
+    type      = string
+    #defaulit = "10.10.0.0/24"
 }
--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,50 @@
 resource "aws_vpc" "main" {
-  cidr_block       = var.vpc_range 
+  cidr_block       = var.vpc_range
   instance_tenancy = "default"
 
   tags = {
     Name = "myvpc111"
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet1_range
+  tags = {
+    Name = "subnet1"
+  }
+}
+resource "aws_subnet" "main1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet2_range
+  tags = {
+    Name = "subnet2"
+  }
+}
+resource "aws_subnet" "main2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet3_range
+  tags = {
+    Name = "subnet3"
+  }
+}
+resource "aws_subnet" "main3" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet4_range
+  tags = {
+    Name = "subnet4"
+  }
+}
+resource "aws_subnet" "main4" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet5_range
+  tags = {
+    Name = "subnet5"
+  }
+}
+resource "aws_subnet" "main5" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet6_range
+  tags = {
+    Name = "subnet6"
   }
 }
